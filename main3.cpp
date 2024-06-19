6a129a87c94a9dcfc696b42236be0734d5d129ba
JoyBed <46244284+JoyBed@users.noreply.github.com>
2024-06-19 07:51:17 +0200

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -116,7 +116,7 @@ int main() {
 
     // Start the decoder
     *jpeg_ctrl = (1 << JPEG_CTRL_ABORT_SHIFT); // Set the ABORT bit
-    *jpeg_ctrl = (1 << JPEG_CTRL_START_SHIFT) | (imageData.size() & JPEG_CTRL_LENGTH_MASK); // Set the START bit and LENGTH field
+    *jpeg_ctrl = ((1 << JPEG_CTRL_START_SHIFT) | (imageData.size() & JPEG_CTRL_LENGTH_MASK)); // Set the START bit and LENGTH field
     printf("Started the decoder");
 
     // Wait for the decoder to finish
