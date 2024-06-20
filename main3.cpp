f7eda964259ace9d7146f5360779ac9e7f8d8c31
JoyBed <46244284+JoyBed@users.noreply.github.com>
2024-06-20 23:11:34 +0200

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -126,14 +126,14 @@ int main() {
     uintptr_t jpegSrcAddr = reinterpret_cast<uintptr_t>(imagePhysAddr);
     *jpeg_src = static_cast<uint32_t>(jpegSrcAddr);
     printf("Set JPEG_SRC register\n");
-    printf("Original value: %zu", imagePhysAddr);
+    printf("Original value: %zd", imagePhysAddr);
     printf("Value from register: %zu", *jpeg_src);
 
     // Set the JPEG_DST register to the address of the RGB565 buffer
     uintptr_t jpegDstAddr = reinterpret_cast<uintptr_t>(rgb565BufferPtr);
     *jpeg_dst = static_cast<uint32_t>(jpegDstAddr);
     printf("Set JPEG_DST register\n");
-    printf("Original value: %zu", *jpegDstAddr);
+    printf("Original value: %zd", *jpegDstAddr);
     printf("Value from register: %zu", *jpeg_dst);
 
     // Start the decoder
